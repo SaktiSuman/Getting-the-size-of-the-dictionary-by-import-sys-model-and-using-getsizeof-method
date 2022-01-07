b19273c2361d7252e147c98a8d607ed7ccbbfc31
Sakti Suman Tripathy <65999640+SaktiSuman@users.noreply.github.com>
2022-01-07 16:05:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+dic1 = {"A": 1, "B": 2, "C": 3}
+dic2 = {"Geek1": "Raju", "Geek2": "Nikhil", "Geek3": "Deepanshu"}
+dic3 = {1: "Lion", 2: "Tiger", 3: "Fox", 4: "Wolf"}
+print("The size of 1st dict is: "+str(sys.getsizeof(dic1))+" bytes")
+print("The size of 2nd dict is: "+str(sys.getsizeof(dic2))+" bytes")
+print("The size of 3rd dict is: "+str(sys.getsizeof(dic3))+" bytes")
